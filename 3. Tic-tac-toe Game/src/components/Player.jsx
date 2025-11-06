97d7ccea7c05e661196a97c579c9ce9447bb34ec
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-07 01:19:58 +0200

    add this to my repository

--- 3. Tic-tac-toe Game/src/components/Player.jsx
+++ 3. Tic-tac-toe Game/src/components/Player.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 export default function Player({ initialName, symbol }) {
+  
   const [playerName, setPlayerName] = useState(initialName);
   const [ isEditing, setIsEditing] = useState(false);
 
@@ -8,10 +9,15 @@ export default function Player({ initialName, symbol }) {
     setIsEditing(isEditing ? false : true);
   }
 
+  function handleChange (event){
+    setPlayerName(event.target.value);
+  }
+
+
   let editablePlayerName = <span className="player-name">{playerName}</span>
 
   if (isEditing){
-    editablePlayerName = <input type="text" required defaultValue={playerName}/>;
+    editablePlayerName = <input type="text" required defaultValue={playerName} onChange={handleChange}/>;
   }
 
   return (
