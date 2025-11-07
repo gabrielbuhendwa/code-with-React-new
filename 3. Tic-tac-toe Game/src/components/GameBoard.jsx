4e6ecc17aa69d5d4bd19774e62e4392933354c0d
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-07 02:18:11 +0200

    add this to my repository

---
+++ 3. Tic-tac-toe Game/src/components/GameBoard.jsx
@@ -0,0 +1,13 @@
+export default function GameBoard(){
+    return <ol id="game-board">
+        <li>
+            <ol>
+                <li></li>
+                <li></li>
+                <li></li> 
+            </ol>
+        </li>
+        <li></li>
+        <li></li>
+    </ol>
+}
