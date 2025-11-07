38be8f8d63263a776dd3cb28a2d5c44c3ba8063a
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-07 16:36:39 +0200

    add this to my repository

--- 3. Tic-tac-toe Game/src/components/GameBoard.jsx
+++ 3. Tic-tac-toe Game/src/components/GameBoard.jsx
@@ -6,6 +6,13 @@ const initialGameBoard = [
 
 export default function GameBoard(){
     return <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}></li>)}
+        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            <ol>
+                {row.map((playerSymbol, colIndex) => (
+                <li key={colIndex}>
+                    <button>{playerSymbol}</button>
+                </li>))}
+            </ol>
+        </li>)}
     </ol>
 }
--- 3. Tic-tac-toe Game/src/components/Player.jsx
+++ 3. Tic-tac-toe Game/src/components/Player.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 export default function Player({ initialName, symbol }) {
   
   const [playerName, setPlayerName] = useState(initialName);
