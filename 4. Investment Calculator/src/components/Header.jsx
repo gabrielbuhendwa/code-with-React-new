3d5835969cdd2bd119e3cf9754d6282f16a3b024
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-11 18:44:04 +0200

    add this to my repository

--- 4. Investment Calculator/src/components/Header.jsx
+++ 4. Investment Calculator/src/components/Header.jsx
@@ -1,5 +1,10 @@
+import logo from '../assets/investment-calculator-logo.png';
+
 export default function Header(){
-    return <header>
-         
-    </header>
+    return (
+        <header id="header">
+            <img src={logo} alt="Logo for money"/>
+            <h1>Investment Calculator</h1>
+        </header>
+    );
 }
