f5e9040bd4498069a633320b8d04364149ea5656
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-29 20:32:52 +0200

    add this to my repository

--- 7. Refs and Portals/src/components/TimerChallenge.jsx
+++ 7. Refs and Portals/src/components/TimerChallenge.jsx
@@ -5,12 +5,15 @@ export default function TimerChallenge({title, targetTime}) {
  const [timerExpired, setTimerExpired] = useState(false);   
 
  function handleStart(){
-    setTimeout(()=> {}, targetTime * 1000);
+    setTimeout(()=> {
+        setTimerExpired(true);
+    }, targetTime * 1000);
  }
 
  return (
  <section className="challenge">
     <h2>{title}</h2>
+    {timerExpired && <p>You lost</p>}
     <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
     </p>
