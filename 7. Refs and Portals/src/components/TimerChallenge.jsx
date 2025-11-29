5e3204af7b2b78c27e0028a49d98f7870eeb3fb3
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-29 20:20:15 +0200

    add this to my repository

--- 7. Refs and Portals/src/components/TimerChallenge.jsx
+++ 7. Refs and Portals/src/components/TimerChallenge.jsx
@@ -1,5 +1,18 @@
-export default function TimerChallenge({title}){
- return <section className="challenge">
+export default function TimerChallenge({title, targetTime}) {
+ return (
+ <section className="challenge">
     <h2>{title}</h2>
+    <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? 's' : ''}
+    </p>
+    <p>
+        <button>
+            Start Challenge
+        </button>
+    </p>
+    <p className="">
+        Time is Runnigng.../Timer inactive
+    </p>
  </section>
-} 
+ );
+}  
