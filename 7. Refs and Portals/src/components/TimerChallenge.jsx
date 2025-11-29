5cf5b08a06ae369e17adfbd07fd025d69d30b0bd
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-29 20:28:06 +0200

    add this to my repository

--- 7. Refs and Portals/src/components/TimerChallenge.jsx
+++ 7. Refs and Portals/src/components/TimerChallenge.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 export default function TimerChallenge({title, targetTime}) {
+
+ const [timerExpired, setTimerExpired] = useState(false);   
+
+ function handleStart(){
+    setTimeout(()=> {}, targetTime * 1000);
+ }
+
  return (
  <section className="challenge">
     <h2>{title}</h2>
