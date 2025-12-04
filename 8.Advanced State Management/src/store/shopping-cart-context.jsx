6c2da72bba41f3a56823934b23204c753c3162ac
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-12-04 05:37:48 +0200

    provide the React context (third update)

--- 8.Advanced State Management/src/store/shopping-cart-context.jsx
+++ 8.Advanced State Management/src/store/shopping-cart-context.jsx
@@ -3,7 +3,5 @@ import { createContext } from 'react';
 // default value for the context
 export const CartContext  = createContext({
   items: [],
-//   addItem: (item) => {},
-//   removeItem: (id) => {},
-//   clear: () => {}, 
+  addItemToCart: () => {},
 });
