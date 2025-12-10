047dc77096f0c7500207935f4bb7bd3a8c24d34f
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-12-10 21:54:25 +0200

    implement the local storage(update)

--- 9.Side Effects useEffect/src/App.jsx
+++ 9.Side Effects useEffect/src/App.jsx
@@ -7,19 +7,19 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find((place) =>place.id === id));
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]); 
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState([storedPlaces]);
 
 
-  useEffect(() => {
+  
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find((place) =>place.id === id));
-
-    setPickedPlaces(storedPlaces);
-  }, []);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
