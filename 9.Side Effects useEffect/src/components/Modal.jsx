8c263b161636fa92fe892429fb5d27a3c3062440
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-12-11 04:06:00 +0200

    project Update

--- 9.Side Effects useEffect/src/components/Modal.jsx
+++ 9.Side Effects useEffect/src/components/Modal.jsx
@@ -1,8 +1,7 @@
 import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-
-const Modal = ({ open, children }) => {
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
 
   useEffect(() => {
@@ -14,8 +13,8 @@ const Modal = ({ open, children }) => {
   }, [open]);
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
