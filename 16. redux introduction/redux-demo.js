ca8388e2db816c14a545d8b900d0fde6322ec8c4
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-12-29 23:54:32 +0200

    Add demonstration code for central state management in Redux

--- 16. redux introduction/redux-demo.js
+++ 16. redux introduction/redux-demo.js
@@ -1,1 +1,24 @@
- 
+//this is how w import redux or any othe things from node module
+const redux = require('redux');
+
+
+//declaring the reducer function and what it should return as a new state 
+const counterReducer = (state = { counter: 0 }, action) => {
+    return {
+        counter: state.counter + 1
+    };
+
+};
+
+//creating the central data (state) store 
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+   const latestState =  store.getState();
+   console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+//store that dispatch an action, action which is a js object 
+store.dispatch({ type: 'increment'});
